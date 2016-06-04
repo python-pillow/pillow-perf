c981ff0d8389f170677c62986a79244a201c3326
homm <homm86@gmail.com>
2016-06-04 03:18:44 +0300

    update test runners

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ Image.LANCZOS = Image.ANTIALIAS
 
 
 class TestCase(object):
-    def __init__(self, runner, *args, **kwargs):
-        self.runner = staticmethod(runner)
+    runner = None
+
+    def __init__(self, *args, **kwargs):
         self.args = args
         if 'desc' in kwargs:
             self.desc = kwargs['desc']
@@ -28,6 +29,9 @@ class TestCase(object):
     def result(self):
         return self._result
 
+    def processed_pixels(self, im):
+        return im.size[0] * im.size[1]
+
     def cleanup(self):
         self._result = None
 
@@ -35,21 +39,15 @@ class TestCase(object):
 class ResizeCase(TestCase):
     runner = staticmethod(Image.Image.resize)
 
-    def __init__(self, size, flt):
-        self.args = (size, flt)
-
     @property
     def desc(self):
         flt = {
-            1: 'bil', 2: 'bic', 3: 'lzs',
+            0: 'ner', 1: 'lzs', 2: 'bil', 3: 'bic',
         }.get(self.args[1], self.args[1])
         return "{size[0]}x{size[1]} {flt}".format(size=self.args[0], flt=flt)
 
 
 class BlurCase(TestCase):
-    def __init__(self, radius):
-        self.args = (radius,)
-
     def runner(self, im, radius):
         return im._new(ImageOps.gaussian_blur(im, radius))
 
@@ -58,36 +56,108 @@ class BlurCase(TestCase):
         return "blur {radius}px".format(radius=self.args[0])
 
 
+class ConvertCase(TestCase):
+    runner = staticmethod(Image.Image.convert)
+
+    def prepare(self, im):
+        im.load()
+        mode = self.args[0]
+        if mode == 'RGBA':
+            im = im.convert('RGBa')
+        return im
+
+    @property
+    def desc(self):
+        return "convert {mode}".format(mode=self.args[0])
+
+    @property
+    def result(self):
+        if self._result.mode == 'RGBa':
+            self._result = self._result.convert('RGBA')
+        return self._result
+
+
+class RotateCase(TestCase):
+    runner = staticmethod(Image.Image.rotate)
+
+    @property
+    def desc(self):
+        flt = {
+            0: 'ner', 1: 'lzs', 2: 'bil', 3: 'bic',
+        }.get(self.args[1], self.args[1])
+        return "rotate {mode} {flt} ".format(mode=self.args[0], flt=flt) + (
+            'ex' if self.args[2] else '  ')
+
+
+class CreateCase(TestCase):
+    times = 10
+
+    def prepare(self, im):
+        pass
+
+    def runner(self, im, *args):
+        for i in range(self.times):
+            Image.new(*args)
+        return im
+
+    @property
+    def desc(self):
+        return "create {mode} {size}".format(
+            mode=self.args[0], size=self.args[1])
+
+    def processed_pixels(self, im):
+        size = self.args[1]
+        return size[0] * size[1] * self.times
+
+
 test_cases = [
-    ResizeCase((16, 16), Image.BILINEAR),
-    ResizeCase((16, 16), Image.BICUBIC),
-    ResizeCase((16, 16), Image.LANCZOS),
-    ResizeCase((320, 180), Image.BILINEAR),
-    ResizeCase((320, 180), Image.BICUBIC),
-    ResizeCase((320, 180), Image.LANCZOS),
-    ResizeCase((1920, 1200), Image.BILINEAR),
-    ResizeCase((1920, 1200), Image.BICUBIC),
-    ResizeCase((1920, 1200), Image.LANCZOS),
-    ResizeCase((7712, 4352), Image.BILINEAR),
-    ResizeCase((7712, 4352), Image.BICUBIC),
-    ResizeCase((7712, 4352), Image.LANCZOS),
+    # ResizeCase((16, 16), Image.NEAREST),
+    # ResizeCase((16, 16), Image.BILINEAR),
+    # ResizeCase((16, 16), Image.BICUBIC),
+    # ResizeCase((16, 16), Image.LANCZOS),
+    # ResizeCase((320, 180), Image.NEAREST),
+    # ResizeCase((320, 180), Image.BILINEAR),
+    # ResizeCase((320, 180), Image.BICUBIC),
+    # ResizeCase((320, 180), Image.LANCZOS),
+    # ResizeCase((1920, 1200), Image.NEAREST),
+    # ResizeCase((1920, 1200), Image.BILINEAR),
+    # ResizeCase((1920, 1200), Image.BICUBIC),
+    # ResizeCase((1920, 1200), Image.LANCZOS),
+    # ResizeCase((7712, 4352), Image.NEAREST),
+    # ResizeCase((7712, 4352), Image.BILINEAR),
+    # ResizeCase((7712, 4352), Image.BICUBIC),
+    # ResizeCase((7712, 4352), Image.LANCZOS),
     # BlurCase(1),
     # BlurCase(10),
     # BlurCase(100),
+    # ConvertCase('RGBa'),
+    # ConvertCase('RGBA'),
+    # RotateCase(10, Image.NEAREST, False),
+    # RotateCase(10, Image.NEAREST, True),
+    # RotateCase(10, Image.BICUBIC, False),
+    # RotateCase(10, Image.BICUBIC, True),
+    # RotateCase(45, Image.NEAREST, False),
+    # RotateCase(45, Image.NEAREST, True),
+    # RotateCase(45, Image.BICUBIC, False),
+    # RotateCase(45, Image.BICUBIC, True),
+    # RotateCase(89.9, Image.NEAREST, False),
+    # RotateCase(89.9, Image.NEAREST, True),
+    # RotateCase(89.9, Image.BICUBIC, False),
+    # RotateCase(89.9, Image.BICUBIC, True),
+    # RotateCase(90, Image.NEAREST, False),
+    # RotateCase(90, Image.NEAREST, True),
+    # RotateCase(90, Image.BICUBIC, False),
+    # RotateCase(90, Image.BICUBIC, True),
+    CreateCase('RGB', (256, 256)),
+    CreateCase('RGB', (1280, 1280)),
+    CreateCase('RGB', (2560, 2560)),
+    CreateCase('RGB', (81920, 80)),
+    CreateCase('RGB', (80, 81920)),
 ]
-
-
-def split(im):
-    im._splitted = im.split()
-    return im._splitted[0]
-
-
-def merge(im):
-    return Image.merge(im.mode, im._splitted)
-
 
 for case in test_cases:
     im = case.prepare(Image.open(sys.argv[1]))
+    pixels = case.processed_pixels(im)
 
     times = []
     for _ in range(11):
@@ -97,7 +167,6 @@ for case in test_cases:
     times.sort()
     median_duration = times[len(times) // 2]
     min_duration = times[0]
-    pixels = im.size[0] * im.size[1]
     print '\r>>> {:14} {:8.5f} s {:8.2f} Mpx/s {:8.2f} Mpx/s'.format(
         case.desc,
         median_duration, pixels / median_duration / 1000 / 1000,
@@ -105,7 +174,8 @@ for case in test_cases:
     )
 
     im = None
-    case.result.save('_pil ' + case.desc + '.png', compress_level=1)
+    if '--save' in sys.argv:
+        case.result.save('_pil ' + case.desc + '.png', compress_level=1)
     case.cleanup()
 
 # time.sleep(10)

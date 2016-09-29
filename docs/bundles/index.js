c01a04deffed2fc7b617802bb1b1409c673a6b13
homm <homm86@gmail.com>
2016-09-29 07:42:40 +0300

    competitions

--- docs/bundles/index.js
+++ docs/bundles/index.js
@@ -47,10 +47,9 @@
 	var adapter = __webpack_require__(1);
 	var data = __webpack_require__(3);
 	
-	var competition = data[0].competitions[0];
-	
 	
 	function createSelect(select, list, callback) {
+	  var elements = [];
 	  select.innerHTML = '';
 	  for (var i = 0; i < list.length; i++) {
 	    var item = list[i];
@@ -61,32 +60,74 @@
 	    })
 	    select.appendChild(document.createElement('li')).appendChild(element);
 	    callback(i, element, item);
+	    elements.push(element);
 	  }
+	
+	  function selectItem(n) {
+	    for (var i = 0; i < elements.length; i++) {
+	      var element = elements[i];
+	      element.className = (n == i) ? 'selected' : '';
+	    }
+	  }
+	
+	  return selectItem;
 	}
 	
 	
 	function populatePresets(chart, presets) {
 	  var select = document.getElementById("select-preset");
 	  
-	  createSelect(select, presets, function(i, element, preset) {
+	  var selectItem = createSelect(select, presets, function(i, element, preset) {
 	    element.innerText = preset.title;
 	    element.addEventListener('click', applyPreset.bind(null, i));
 	  });
 	
 	  function applyPreset(n) {
+	    selectItem(n);
 	    adapter.applyPreset(chart, presets[n].set);
 	  }
 	  return applyPreset;
 	}
 	
 	
+	function populateCompetitions(competitions) {
+	  var select = document.getElementById("select-competition");
+	  
+	  var selectItem = createSelect(select, competitions, function(i, element, competition) {
+	    element.innerText = competition.title;
+	    element.addEventListener('click', applyCompetition.bind(null, i));
+	  });
+	
+	  function applyCompetition(n) {
+	    var competition = competitions[n];
+	    var chart = adapter.chartForCompetition(
+	      document.getElementById("chart-container"),
+	      competition
+	    );
+	
+	    selectItem(n);
+	
+	    if (competition.presets) {
+	      var applyPreset = populatePresets(chart, competition.presets);
+	
+	      for (var i = 0; i < competition.presets.length; i++) {
+	        if (competition.presets[i].default) {
+	          applyPreset(i);
+	          break;
+	        }
+	      }
+	    } else {
+	      populatePresets(chart, []);
+	    }
+	  }
+	  return applyCompetition;
+	}
+	
+	
 	document.addEventListener("DOMContentLoaded", function(){
-	  var chart = adapter.chartForCompetition(
-	    document.getElementById("chart-container"),
-	    competition
-	  );
-	  var applyPreset = populatePresets(chart, competition.presets);
-	  applyPreset(4);
+	  
+	  var applyCompetition = populateCompetitions(data[0].competitions);
+	  applyCompetition(0);
 	});
 
 
@@ -11081,7 +11122,8 @@
 							"pillow-3.4",
 							"pillow-simd-3.4-sse4",
 							"pillow-simd-3.4-avx2"
-						]
+						],
+						"default": true
 					},
 					{
 						"title": "Pillow SIMD SSE4 progress",

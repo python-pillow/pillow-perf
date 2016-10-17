49127a11e09cc97a1faf6acf04da88556a1a1506
homm <homm86@gmail.com>
2016-10-17 11:21:50 +0300

    switcher for avx2 tests

--- auto/run.sh
+++ auto/run.sh
@@ -24,33 +24,39 @@ pushd Pillow
   }
 
   checkout 2.6.2
-  ../../testsuite/run.py scale "$@"
+  ../../testsuite/run.py scale "${@:2}"
   
   checkout 2.7.0
-  ../../testsuite/run.py scale "$@"
+  ../../testsuite/run.py scale "${@:2}"
 
   checkout 3.3.3
-  ../../testsuite/run.py scale "$@"
+  ../../testsuite/run.py scale "${@:2}"
 
   checkout 3.4.1
-  ../../testsuite/run.py scale "$@"
+  ../../testsuite/run.py scale "${@:2}"
 
   checkout v3.2.0.post3 -msse4
-  ../../testsuite/run.py scale "$@"
+  ../../testsuite/run.py scale "${@:2}"
 
-  checkout v3.2.0.post3 -mavx2
-  ../../testsuite/run.py scale "$@"
+  if [ "$1" != "no" ]; then
+    checkout v3.2.0.post3 -mavx2
+    ../../testsuite/run.py scale "${@:2}"
+  fi
 
   checkout v3.3.3.post0 -msse4
-  ../../testsuite/run.py scale "$@"
+  ../../testsuite/run.py scale "${@:2}"
 
-  checkout v3.3.3.post0 -mavx2
-  ../../testsuite/run.py scale "$@"
+  if [ "$1" != "no" ]; then
+    checkout v3.3.3.post0 -mavx2
+    ../../testsuite/run.py scale "${@:2}"
+  fi
 
   checkout v3.4.1.post1 -msse4
-  ../../testsuite/run.py scale "$@"
+  ../../testsuite/run.py scale "${@:2}"
 
-  checkout v3.4.1.post1 -mavx2
-  ../../testsuite/run.py scale "$@"
+  if [ "$1" != "no" ]; then
+    checkout v3.4.1.post1 -mavx2
+    ../../testsuite/run.py scale "${@:2}"
+  fi
 
 popd

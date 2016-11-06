9fe41c46b905d3d973611cdda6e8353d4238ae8f
homm <homm86@gmail.com>
2016-11-06 20:03:44 +0300

    use BaseScaleCase inwand_scale

--- testsuite/cases/wand_scale.py
+++ testsuite/cases/wand_scale.py
@@ -2,39 +2,14 @@
 
 from __future__ import print_function, unicode_literals, absolute_import
 
-from .base import rpartial
+from .base import rpartial, BaseScaleCase
 from .wand import WandTestCase
 
 
-class ScaleCase(WandTestCase):
-    def handle_args(self, dest_size, filter, hpass=True, vpass=True):
-        self.dest_size = dest_size
-        self.filter = filter
-        self.hpass = hpass
-        self.vpass = vpass
-
+class ScaleCase(BaseScaleCase, WandTestCase):
     def runner(self, im):
-        self.update_dest_size(im)
         with im.clone() as im:
-            im.resize(
-                self.calc_dest_size[0],
-                self.calc_dest_size[1],
-                self.filter,
-            )
-
-    def readable_args(self):
-        return [
-            "x".join(map(str, self.calc_dest_size)),
-            self.filter_ids.get(self.filter, self.filter),
-        ]
-
-    def update_dest_size(self, im):
-        self.calc_dest_size = [
-            (int(round(self.dest_size * im.width))
-                if self.hpass else im.width),
-            (int(round(self.dest_size * im.height))
-                if self.vpass else im.height),
-        ]
+            im.resize(self.dest_size[0], self.dest_size[1], self.filter)
 
 
 cases = [

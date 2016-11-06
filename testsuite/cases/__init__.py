373632d0f3af5f4b9fa5cace65655cbb70526fb9
homm <homm86@gmail.com>
2016-11-06 17:18:00 +0300

    cv2_load

--- testsuite/cases/__init__.py
+++ testsuite/cases/__init__.py
@@ -4,6 +4,7 @@ from __future__ import print_function, unicode_literals, absolute_import
 
 from importlib import import_module
 
+
 def collect_testsuites():
     return [
         "load", "convert", "composition", "rotate_right",
@@ -11,6 +12,8 @@ def collect_testsuites():
 
         "wand_load", "wand_rotate_right",
         "wand_resize", "wand_scale",
+
+        "cv2_load",
     ]
 
 

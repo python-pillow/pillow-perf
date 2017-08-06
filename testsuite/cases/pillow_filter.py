a5fcedccb8a6f6798bac66597dc535c1fa47d85d
Alexander <homm86@gmail.com>
2017-08-07 00:58:55 +0300

    Pillow filter tests

---
+++ testsuite/cases/pillow_filter.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from __future__ import print_function, unicode_literals, absolute_import
+
+from PIL import ImageFilter
+
+from .base import rpartial
+from .pillow import PillowTestCase
+
+
+class FilterCase(PillowTestCase):
+    def handle_args(self, filter):
+        self.filter = filter
+
+    def runner(self, im):
+        im.filter(self.filter)
+
+    def readable_args(self):
+        return [self.filter.name]
+
+
+cases = [
+    rpartial(FilterCase, ImageFilter.SMOOTH),
+    rpartial(FilterCase, ImageFilter.SHARPEN),
+    rpartial(FilterCase, ImageFilter.SMOOTH_MORE),
+]

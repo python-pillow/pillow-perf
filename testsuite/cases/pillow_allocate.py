547401fee445a158407fc20ea4d85e320f4a6f49
Alexander <homm86@gmail.com>
2017-08-08 23:56:33 +0300

    Additional test for allocation

--- testsuite/cases/pillow_allocate.py
+++ testsuite/cases/pillow_allocate.py
@@ -2,8 +2,9 @@
 
 from __future__ import print_function, unicode_literals, absolute_import
 
-from .base import rpartial, BaseAllocateCase
-from .pillow import  Image
+from .base import (rpartial, BaseAllocateCase, BaseSplitCase,
+                   BaseGetBandCase, BaseMergeCase)
+from .pillow import Image, PillowTestCase
 
 
 class AllocateCase(BaseAllocateCase):
@@ -11,9 +12,32 @@ class AllocateCase(BaseAllocateCase):
         Image.new(self.mode, self.size)
 
 
+class SplitCase(BaseSplitCase, PillowTestCase):
+    def runner(self, im):
+        im.split()
+
+
+class GetBandCase(BaseGetBandCase, PillowTestCase):
+    def runner(self, im):
+        im.split()[self.band]
+
+
+class MergeCase(BaseMergeCase, PillowTestCase):
+    def runner(self, bands):
+        Image.merge(self.mode, bands)
+
+
 cases = [
     rpartial(AllocateCase, 'L'),
     rpartial(AllocateCase, 'LA'),
     rpartial(AllocateCase, 'RGB'),
     rpartial(AllocateCase, 'RGBA'),
+    rpartial(SplitCase, 'LA'),
+    rpartial(SplitCase, 'RGB'),
+    rpartial(SplitCase, 'RGBA'),
+    rpartial(GetBandCase, 'RGB', 0),
+    rpartial(GetBandCase, 'RGBA', 3),
+    rpartial(MergeCase, 'LA'),
+    rpartial(MergeCase, 'RGB'),
+    rpartial(MergeCase, 'RGBA'),
 ]

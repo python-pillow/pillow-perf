949f6a4b0f5165f467fd634ff7535ef71952f0bf
Alexander <homm86@gmail.com>
2017-08-11 03:30:54 +0300

    pillow save case

--- testsuite/cases/pillow_load.py
+++ testsuite/cases/pillow_load.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function, unicode_literals, absolute_import
 
+from io import BytesIO
+
 from PIL import Image
 
 from .base import rpartial, root, BaseTestCase
@@ -20,6 +22,24 @@ class LoadCase(BaseTestCase):
         return ["{} load".format(self.filetype)]
 
 
+class SaveCase(BaseTestCase):
+    def handle_args(self, filetype, filename):
+        self.filetype = filetype
+        self.filename = filename
+
+    def create_test_data(self):
+        im = Image.open(root('resources', self.filename))
+        im.load()
+        return [im]
+
+    def runner(self, im):
+        im.save(BytesIO(), format=self.filetype)
+
+    def readable_args(self):
+        return ["{} save".format(self.filetype)]
+
+
 cases = [
     rpartial(LoadCase, 'Jpeg', 'pineapple.jpeg'),
+    rpartial(SaveCase, 'Jpeg', 'pineapple.jpeg'),
 ]

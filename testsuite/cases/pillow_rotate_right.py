64194516a2dadbaf4316ff6cfff91d82589680ba
Alexander <homm86@gmail.com>
2017-09-23 12:22:46 +0300

    add Transverse to all rotate_right tests

--- testsuite/cases/pillow_rotate_right.py
+++ testsuite/cases/pillow_rotate_right.py
@@ -9,30 +9,31 @@ from .pillow import PillowTestCase
 
 
 class RotateRightCase(PillowTestCase):
-    def handle_args(self, name, transposition):
+    def handle_args(self, name, runner):
         self.name = name
-        self.transposition = transposition
-
-    def runner(self, im):
-        for trans in self.transposition:
-            im = im.transpose(trans)
+        self.runner = runner
 
     def readable_args(self):
         return [self.name]
 
 
 cases = [
-    rpartial(RotateRightCase, 'Flop', [Image.FLIP_LEFT_RIGHT]),
-    rpartial(RotateRightCase, 'Flip', [Image.FLIP_TOP_BOTTOM]),
-    rpartial(RotateRightCase, 'Rotate 90', [Image.ROTATE_90]),
-    rpartial(RotateRightCase, 'Rotate 180', [Image.ROTATE_180]),
-    rpartial(RotateRightCase, 'Rotate 270', [Image.ROTATE_270]),
+    rpartial(RotateRightCase, 'Flop',
+        lambda im: im.transpose(Image.FLIP_LEFT_RIGHT)),
+    rpartial(RotateRightCase, 'Flip',
+        lambda im: im.transpose(Image.FLIP_TOP_BOTTOM)),
+    rpartial(RotateRightCase, 'Rotate 90',
+        lambda im: im.transpose(Image.ROTATE_90)),
+    rpartial(RotateRightCase, 'Rotate 180',
+        lambda im: im.transpose(Image.ROTATE_180)),
+    rpartial(RotateRightCase, 'Rotate 270',
+        lambda im: im.transpose(Image.ROTATE_270)),
     rpartial(RotateRightCase, 'Transpose',
-        [Image.TRANSPOSE]
+        (lambda im: im.transpose(Image.TRANSPOSE))
         if hasattr(Image, 'TRANSPOSE')
-        else [Image.ROTATE_90, Image.FLIP_LEFT_RIGHT]),
+        else lambda im: im.transpose(Image.ROTATE_90).transpose(Image.FLIP_TOP_BOTTOM)),
     rpartial(RotateRightCase, 'Transverse',
-        [Image.TRANSVERSE]
+        (lambda im: im.transpose(Image.TRANSVERSE))
         if hasattr(Image, 'TRANSVERSE')
-        else [Image.ROTATE_270, Image.FLIP_LEFT_RIGHT]),
+        else lambda im: im.transpose(Image.ROTATE_270).transpose(Image.FLIP_TOP_BOTTOM)),
 ]

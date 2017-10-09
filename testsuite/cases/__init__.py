eb2323d67c3522140d2ab2b39da72cef07212872
Alexander <homm86@gmail.com>
2017-10-10 00:27:00 +0300

    pgmagick_rotate_right

--- testsuite/cases/__init__.py
+++ testsuite/cases/__init__.py
@@ -14,7 +14,7 @@ def collect_test_suites():
         "wand_rotate_right", "wand_scale", "wand_blur",
 
         "pgmagick_load", "pgmagick_convert", "pgmagick_composition",
-        "pgmagick_scale", "pgmagick_blur",
+        "pgmagick_rotate_right", "pgmagick_scale", "pgmagick_blur",
 
         "cv2_load", "cv2_rotate_right", "cv2_scale", "cv2_blur", "cv2_filter",
     ]
---
+++ testsuite/cases/pgmagick_rotate_right.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+from __future__ import print_function, unicode_literals, absolute_import
+
+from .base import rpartial
+from .pgmagick import Image, PgmagickTestCase
+
+
+class RotateRightCase(PgmagickTestCase):
+    def handle_args(self, name, operation):
+        self.name = name
+        self.operation = operation
+
+    def runner(self, im):
+        im = Image(im)
+        self.operation(im)
+
+    def readable_args(self):
+        return [self.name]
+
+
+def transpose(im, op2):
+    im.rotate(90.0)
+    op2(im)
+
+cases = [
+    rpartial(RotateRightCase, 'Flop', Image.flop),
+    rpartial(RotateRightCase, 'Flip', Image.flip),
+    rpartial(RotateRightCase, 'Rotate 90',
+             rpartial(Image.rotate, 270.0)),
+    rpartial(RotateRightCase, 'Rotate 180',
+             rpartial(Image.rotate, 180.0)),
+    rpartial(RotateRightCase, 'Rotate 270',
+             rpartial(Image.rotate, 90.0)),
+    rpartial(RotateRightCase, 'Transpose', rpartial(transpose, Image.flop)),
+    rpartial(RotateRightCase, 'Transverse', rpartial(transpose, Image.flip)),
+]

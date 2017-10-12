f8dfc6b75c867c2ebffb598d5fd9d1581ac9dd71
Alexander <homm86@gmail.com>
2017-10-12 13:39:22 +0300

    version independent resize filters, comment for pillow

--- testsuite/cases/vips.py
+++ testsuite/cases/vips.py
@@ -5,10 +5,24 @@ from __future__ import print_function, unicode_literals, absolute_import
 import os
 
 import pyvips
-from pyvips import Image, Interpolate
+from pyvips import Image, Interpolate, at_least_libvips
 
+from .base import BaseTestCase
 
 # Make VIPS single threaded like other libraries are.
 os.environ['VIPS_CONCURRENCY'] = '1'
 
 pyvips.cache_set_max(0)
+
+
+class VipsTestCase(BaseTestCase):
+    @classmethod
+    def resize(cls, im, *args, **kwargs):
+        kernel = kwargs.pop('kernel', 'cubic')
+        if at_least_libvips(8, 3):
+            # resize with kernel added in 8.3
+            kwargs['kernel'] = kernel
+        else:
+            kwargs['interpolate'] = Interpolate.new('bi' + kernel)
+
+        return im.resize(*args, **kwargs)

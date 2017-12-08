f5c199da1a78ebc7f5591149bfbcbf2a67b7a50e
Andrew Murray <radarhere@users.noreply.github.com>
2017-12-09 07:07:44 +1100

    Moved path into common variable

--- auto/run.sh
+++ auto/run.sh
@@ -37,68 +37,70 @@ pushd Pillow
     "$@"
   }
 
+  RUN=../../testsuite/run.py
+  
   pillow_checkout 2.6.2
-  ../../testsuite/run.py scale filter full_cycle -n5 "${@:2}"
-  ../../testsuite/run.py load composition rotate_right "${@:2}"
+  $RUN scale filter full_cycle -n5 "${@:2}"
+  $RUN load composition rotate_right "${@:2}"
   
   pillow_checkout 2.7.0
-  ../../testsuite/run.py scale blur full_cycle -n5 "${@:2}"
-  ../../testsuite/run.py convert rotate_right "${@:2}"
+  $RUN scale blur full_cycle -n5 "${@:2}"
+  $RUN convert rotate_right "${@:2}"
 
   pillow_checkout 3.3.3
-  ../../testsuite/run.py scale -n5 "${@:2}"
-  ../../testsuite/run.py convert "${@:2}"
+  $RUN scale -n5 "${@:2}"
+  $RUN convert "${@:2}"
 
   pillow_checkout 3.4.2
-  ../../testsuite/run.py scale -n5 "${@:2}"
+  $RUN scale -n5 "${@:2}"
   
   pillow_checkout 4.3.0
-  ../../testsuite/run.py scale filter -n5 "${@:2}"
-  ../../testsuite/run.py load convert composition rotate_right "${@:2}"
-  MM ../../testsuite/run.py scale filter -n5 "${@:2}"
-  MM ../../testsuite/run.py load convert composition rotate_right "${@:2}"
+  $RUN scale filter -n5 "${@:2}"
+  $RUN load convert composition rotate_right "${@:2}"
+  MM $RUN scale filter -n5 "${@:2}"
+  MM $RUN load convert composition rotate_right "${@:2}"
   
 
   pillow_checkout v3.2.0.post3 -msse4
-  ../../testsuite/run.py scale blur "${@:2}"
+  $RUN scale blur "${@:2}"
 
   if [ "$1" != "no" ]; then
     pillow_checkout v3.2.0.post3 -mavx2
-    ../../testsuite/run.py scale "${@:2}"
+    $RUN scale "${@:2}"
   fi
 
   pillow_checkout v3.3.3.post0 -msse4
-  ../../testsuite/run.py scale convert composition "${@:2}"
+  $RUN scale convert composition "${@:2}"
 
   if [ "$1" != "no" ]; then
     pillow_checkout v3.3.3.post0 -mavx2
-    ../../testsuite/run.py scale convert composition "${@:2}"
+    $RUN scale convert composition "${@:2}"
   fi
 
   pillow_checkout v3.4.1.post2 -msse4
-  ../../testsuite/run.py scale "${@:2}"
+  $RUN scale "${@:2}"
 
   if [ "$1" != "no" ]; then
     pillow_checkout v3.4.1.post2 -mavx2
-    ../../testsuite/run.py scale "${@:2}"
+    $RUN scale "${@:2}"
   fi
 
   pillow_checkout v4.3.0.post0 -msse4
-  ../../testsuite/run.py scale blur full_cycle convert composition filter "${@:2}"
-  MM ../../testsuite/run.py scale blur full_cycle convert composition filter "${@:2}"
+  $RUN scale blur full_cycle convert composition filter "${@:2}"
+  MM $RUN scale blur full_cycle convert composition filter "${@:2}"
 
   if [ "$1" != "no" ]; then
     pillow_checkout v4.3.0.post0 -mavx2
-    ../../testsuite/run.py scale convert full_cycle composition filter "${@:2}"
-    MM ../../testsuite/run.py scale convert full_cycle composition filter "${@:2}"
+    $RUN scale convert full_cycle composition filter "${@:2}"
+    MM $RUN scale convert full_cycle composition filter "${@:2}"
   fi
 
-  ../../testsuite/run.py wand_scale wand_full_cycle -n5 "${@:2}"
-  ../../testsuite/run.py wand_blur -n3 "${@:2}"
-  ../../testsuite/run.py wand_load wand_convert wand_composition wand_rotate_right "${@:2}"
+  $RUN wand_scale wand_full_cycle -n5 "${@:2}"
+  $RUN wand_blur -n3 "${@:2}"
+  $RUN wand_load wand_convert wand_composition wand_rotate_right "${@:2}"
 
-  ../../testsuite/run.py cv2_scale cv2_blur cv2_full_cycle -n5 "${@:2}"
-  ../../testsuite/run.py cv2_load cv2_filter cv2_rotate_right "${@:2}"
+  $RUN cv2_scale cv2_blur cv2_full_cycle -n5 "${@:2}"
+  $RUN cv2_load cv2_filter cv2_rotate_right "${@:2}"
 
-  ../../testsuite/run.py vips_load vips_full_cycle "${@:2}"
+  $RUN vips_load vips_full_cycle "${@:2}"
 popd

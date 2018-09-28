645268e88c848f819def465d38ba43731420515c
Alexander <homm86@gmail.com>
2018-09-28 15:15:51 +0300

    FastVideo SDK test case
    ref: https://habr.com/post/424575/

--- testsuite/cases/pillow_full_cycle.py
+++ testsuite/cases/pillow_full_cycle.py
@@ -5,31 +5,32 @@ from __future__ import print_function, unicode_literals, absolute_import
 from io import BytesIO
 
 from .base import rpartial, root, FullCycleBaseCase
-from .pillow import Image, PillowTestCase
+from .pillow import Image, ImageFilter, PillowTestCase
 
 
 class FullCycleCase(FullCycleBaseCase, PillowTestCase):
+    def handle_args(self, *args, **kwargs):
+        super(FullCycleCase, self).handle_args(*args, **kwargs)
+        im = Image.open(root('resources', self.filename))
+        assert im.size == self.size
+
     def runner(self):
         im = Image.open(root('resources', self.filename))
 
         if self.level > 0:
-            im = im.transpose(Image.ROTATE_270)
+            size = (int(im.size[0] * 0.5 + 0.5),
+                    int(im.size[1] * 0.5 + 0.5))
+            im = self.resize(im, size, Image.LANCZOS)
 
         if self.level > 1:
-            size = (int(im.size[0] * 0.4 + 0.5),
-                    int(im.size[1] * 0.4 + 0.5))
-            im = self.resize(im, size, Image.BICUBIC)
-
-        if self.level > 2:
-            im = self.gaussian_blur(im, 4)
+            im = im.filter(ImageFilter.SHARPEN)
 
         im.save(BytesIO(), format=self.filetype, quality=85)
-        # im.save('../_out.{}.pillow.png'.format(self.level))
+        # im.save('../_out.{}.pillow.jpeg'.format(self.level))
 
 
 cases = [
-    rpartial(FullCycleCase, 0, 'Load+save', 'pineapple.jpeg', 'JPEG'),
-    rpartial(FullCycleCase, 1, '+transpose', 'pineapple.jpeg', 'JPEG'),
-    rpartial(FullCycleCase, 2, '+resize', 'pineapple.jpeg', 'JPEG'),
-    rpartial(FullCycleCase, 3, '+blur', 'pineapple.jpeg', 'JPEG'),
+    rpartial(FullCycleCase, 0, 'Load+save', 'pineapple.1920.jpeg', 'JPEG'),
+    rpartial(FullCycleCase, 1, '+resize', 'pineapple.1920.jpeg', 'JPEG'),
+    rpartial(FullCycleCase, 2, '+sharp', 'pineapple.1920.jpeg', 'JPEG'),
 ]

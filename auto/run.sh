bfcf8d8af6e35fd057a1a488f595363a7ed1bf3e
Alexander <homm86@gmail.com>
2020-01-27 02:26:18 +0300

    remove old pillow versions from auto tests
    add pillow and pillow-simd 7.0

--- auto/run.sh
+++ auto/run.sh
@@ -22,7 +22,8 @@ pushd Pillow
 
   function pillow_checkout {
     git checkout -f $1
-    touch ./_imaging.c
+    # Force rebuild
+    mkdir -p src && touch ./_imaging.c ./src/_imaging.c
     CFLAGS="$2" python ./setup.py develop > build.log 2>&1
     grep --color=always "error:" build.log && exit || true
     echo "========================="
@@ -30,6 +31,8 @@ pushd Pillow
     echo "========================="
   }
 
+  # Tests with custom memory management:
+  # MM $RUN scale filter -n5 "${@:2}"
   function MM {
     LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so.1 \
     PILLOW_BLOCK_SIZE=128k \
@@ -39,63 +42,43 @@ pushd Pillow
   }
 
   RUN=../../testsuite/run.py
-  
-  pillow_checkout 2.6.2
-  $RUN scale filter full_cycle -n5 "${@:2}"
-  $RUN load composition rotate_right "${@:2}"
-  
-  pillow_checkout 2.7.0
-  $RUN scale blur full_cycle -n5 "${@:2}"
-  $RUN convert rotate_right "${@:2}"
 
-  pillow_checkout 3.3.3
-  $RUN scale -n5 "${@:2}"
-  $RUN convert "${@:2}"
 
-  pillow_checkout 3.4.2
-  $RUN scale -n5 "${@:2}"
-  
+  # ============
+  # Pillow 4.3.0
+  # ============
   pillow_checkout 4.3.0
-  $RUN scale filter -n5 "${@:2}"
+  $RUN scale blur filter full_cycle -n5 "${@:2}"
   $RUN load convert composition rotate_right "${@:2}"
-  MM $RUN scale filter -n5 "${@:2}"
-  MM $RUN load convert composition rotate_right "${@:2}"
-  
-
-  pillow_checkout v3.2.0.post3 -msse4
-  $RUN scale blur "${@:2}"
-
-  if [ "$1" != "no" ]; then
-    pillow_checkout v3.2.0.post3 -mavx2
-    $RUN scale "${@:2}"
-  fi
-
-  pillow_checkout v3.3.3.post0 -msse4
-  $RUN scale convert composition "${@:2}"
-
-  if [ "$1" != "no" ]; then
-    pillow_checkout v3.3.3.post0 -mavx2
-    $RUN scale convert composition "${@:2}"
-  fi
-
-  pillow_checkout v3.4.1.post2 -msse4
-  $RUN scale "${@:2}"
-
-  if [ "$1" != "no" ]; then
-    pillow_checkout v3.4.1.post2 -mavx2
-    $RUN scale "${@:2}"
-  fi
 
   pillow_checkout v4.3.0.post0 -msse4
-  $RUN scale blur full_cycle convert composition filter "${@:2}"
-  MM $RUN scale blur full_cycle convert composition filter "${@:2}"
+  $RUN scale blur filter full_cycle load convert composition rotate_right "${@:2}"
 
   if [ "$1" != "no" ]; then
     pillow_checkout v4.3.0.post0 -mavx2
-    $RUN scale convert full_cycle composition filter "${@:2}"
-    MM $RUN scale convert full_cycle composition filter "${@:2}"
+    $RUN scale filter full_cycle convert composition "${@:2}"
   fi
 
+
+  # ============
+  # Pillow 7.0.0
+  # ============
+  pillow_checkout 7.0.0
+  $RUN scale blur filter full_cycle lut -n5 "${@:2}"
+  $RUN load convert composition rotate_right "${@:2}"
+
+  pillow_checkout v7.0.0.post3 -msse4
+  $RUN scale blur filter full_cycle lut load convert composition rotate_right "${@:2}"
+
+  if [ "$1" != "no" ]; then
+    pillow_checkout v7.0.0.post3 -mavx2
+    $RUN scale filter full_cycle lut convert composition "${@:2}"
+  fi
+
+
+  # ===========
+  # Competitors
+  # ===========
   $RUN wand_scale wand_full_cycle -n5 "${@:2}"
   $RUN wand_blur wand_lut -n3 "${@:2}"
   $RUN wand_load wand_convert wand_composition wand_rotate_right "${@:2}"
@@ -108,4 +91,5 @@ pushd Pillow
   $RUN cv2_load cv2_filter cv2_rotate_right "${@:2}"
 
   $RUN vips_load vips_full_cycle "${@:2}"
+
 popd

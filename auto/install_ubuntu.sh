841375cdb065d2279bfdc2b7791fd8a02cfa720d
Alexander <homm86@gmail.com>
2020-02-02 02:18:54 +0300

    update deps for custom libvips

--- auto/install_ubuntu.sh
+++ auto/install_ubuntu.sh
@@ -1,7 +1,7 @@
 apt-get update
-apt-get install -y htop rsync \
+apt-get install -y \
     build-essential git ccache libjemalloc1 \
     libmagickwand-dev libboost-python-dev libgraphicsmagick++1-dev \
-    libvips-dev libglib2.0-dev \
     python3-openssl python3-dev python3-venv \
-    libjpeg-dev libwebp-dev libtiff-dev
+    libjpeg-dev libwebp-dev libtiff-dev \
+    libvips-dev libglib2.0-dev libexpat1-dev libexif-dev liblcms2-dev liborc-0.4-dev

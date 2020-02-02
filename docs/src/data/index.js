cb401eb7f366314bbf3f536edf6153d45db82278
Alexander <homm86@gmail.com>
2020-02-02 04:02:35 +0300

    add c5.large instance

--- docs/src/data/index.js
+++ docs/src/data/index.js
@@ -155,9 +155,8 @@ function fillSystemWithMeta(system) {
 
 module.exports = {
   systems: [
-	  fillSystemWithMeta(require("./c4.large.json")),
-	  fillSystemWithMeta(require("./c3.large.json")),
-	  fillSystemWithMeta(require("./macbook.2019.json")),
+	  fillSystemWithMeta(require("./c5.large.json")),
 	  fillSystemWithMeta(require("./m5a.large.json")),
+	  fillSystemWithMeta(require("./macbook.2019.json")),
 	],
 };
